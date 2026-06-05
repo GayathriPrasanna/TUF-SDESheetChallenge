09c672a0826e5ca9339ee4206ededcc772134194
Chilakalapalli GayathriPrasanna <gayathri162004@gmail.com>
2026-06-05 20:31:24 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (81.22%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,19 +1,40 @@
+// public class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         String result = "";
+
+//         for (int i = 0; i < strs[0].length(); i++) {
+//             char ch = strs[0].charAt(i);
+
+//             for (int j = 1; j < strs.length; j++) {
+//                 if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
+//                     return result;
+//                 }
+//             }
+
+//             result += ch;
+//         }
+
+//         return result;
+//     }
+// }
+
+
+
+
 public class Solution {
     public String longestCommonPrefix(String[] strs) {
-        String result = "";
+        if (strs == null || strs.length == 0) return "";
 
-        for (int i = 0; i < strs[0].length(); i++) {
-            char ch = strs[0].charAt(i);
+        String prefix = strs[0];
 
-            for (int j = 1; j < strs.length; j++) {
-                if (i >= strs[j].length() || strs[j].charAt(i) != ch) {
-                    return result;
-                }
+        for (int i = 1; i < strs.length; i++) {
+            while (!strs[i].startsWith(prefix)) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+
+                if (prefix.isEmpty()) return "";
             }
-
-            result += ch;
         }
 
-        return result;
+        return prefix;
     }
 }
